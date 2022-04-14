4455c3cbd1166a6c5d527457e1d78f414018cda8
Bo Lei <blei1@andrew.cmu.edu>
2022-04-14 13:35:11 -0400

    Optimize MILP with GUROBI

--- select/optimization.py
+++ select/optimization.py
@@ -1,11 +1,9 @@
-import itertools
-
 import numpy as np
 import cvxpy as cp
-from itertools import combinations
+import itertools
 
 
-def optimize(sim_matrix: np.ndarray, k: int) -> np.ndarray:
+def milp_optimize(sim_matrix: np.ndarray, k: int) -> np.ndarray:
     """
     Optimize a mixed-integer linear program to find a subset of size k such that
     the sum of maximum similarity between the subset and the rest of the set is
@@ -26,22 +24,23 @@ def optimize(sim_matrix: np.ndarray, k: int) -> np.ndarray:
     constraints = [cp.sum(x) == k,
                    cp.sum(b, axis=1) == np.ones(n)]
     for j in range(n):
-        constraints.append(z >= cp.multiply(sim_matrix[:,j], x[j]))
-        constraints.append(z <= cp.multiply(sim_matrix[:,j], x[j]) + (1-b[:,j]))
+        constraints.append(z >= cp.multiply(sim_matrix[:, j], x[j]))
+        constraints.append(z <= cp.multiply(sim_matrix[:, j], x[j]) + 2*(1-b[:, j]))
 
     obj = cp.Maximize(cp.sum(z))
     prob = cp.Problem(obj, constraints)
-    prob.solve(solver='GLPK_MI')
-    return np.where(x.value == 1)
+    prob.solve(solver=cp.GUROBI)
+    return np.where(x.value == 1)[0]
+
 
 def _brute_force_search(sim_matrix, k):
     best_sim = 0
     selection = tuple()
     n = sim_matrix.shape[0]
     indices = set(np.arange(n))
-    for subset in combinations(indices, k):
+    for subset in itertools.combinations(indices, k):
         remainder = tuple(indices.difference(subset))
-        sim = sim_matrix[subset, :][:, remainder].max(axis = 0).sum()
+        sim = sim_matrix[subset, :][:, remainder].max(axis=0).sum()
         if sim > best_sim:
             best_sim = sim
             selection = subset
@@ -49,16 +48,19 @@ def _brute_force_search(sim_matrix, k):
 
 
 if __name__ == '__main__':
-    np.random.seed(42)
-    for n, k in itertools.product([20], [1, 2, 3, 5]):
+    np.random.seed(0)
+    import time
+    for n, k in itertools.product([10, 20, 50], [1, 2, 5]):
         print(n, k)
         sim_mat = np.random.rand(n, n)
         for i in range(sim_mat.shape[0]):
-            sim_mat[i,i] = 1
+            sim_mat[i, i] = 1
             for j in range(i, sim_mat.shape[1]):
-                sim_mat[i,j] = sim_mat[j,i]
-        res_milp = optimize(sim_mat, k)
-        print("finish optimize")
+                sim_mat[i, j] = sim_mat[j, i]
+        start = time.time()
+        res_milp = milp_optimize(sim_mat, k)
+        print(f"complete MILP optimization in {time.time() - start:.3f} s")
+        start = time.time()
         res_bf = _brute_force_search(sim_mat, k)
-        print("finish brute force")
+        print(f"complete brute force search in {time.time() - start:.3f} s")
         assert np.all(res_milp == res_bf)
