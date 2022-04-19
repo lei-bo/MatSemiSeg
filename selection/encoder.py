67eb1670a4b6dd2d6a9e7f56b8b08fc1e16b01f4
Bo Lei <blei1@andrew.cmu.edu>
2022-04-19 12:04:35 -0400

    Implement VLAD encodings

---
+++ selection/encoder.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import vgg16
+
+
+class hypercolumn_vgg(nn.Module):
+    """
+    Hypercolumn representation of image at pixel level using VGG16
+    convolutional blocks as feature extractor. Feature maps at different
+    convolution blocks are upsampled using bilinear upsampling to ensure the
+    spatial resolution is the same to the input image. The output is a tensor
+    with shape NCHW.
+    """
+    def __init__(self, n_conv_blocks=3):
+        """
+        :param n_conv_blocks: number of VGG16 convolutional blocks used for
+        feature extraction.
+        """
+        super(hypercolumn_vgg, self).__init__()
+        features = list(vgg16(pretrained=True).features)
+        self.features = nn.ModuleList(features)
+        self.n_conv_blocks = n_conv_blocks
+        self.feature_maps_index = [3, 8, 15, 22, 29]
+
+    def forward(self, x):
+        feature_maps_index = self.feature_maps_index[:self.n_conv_blocks]
+        size = (x.size(2), x.size(3))
+        feature_maps = []
+        for i, model in enumerate(self.features):
+            x = model(x)
+            if i in feature_maps_index:
+                feature_maps.append(x)
+            if i == feature_maps_index[-1]:
+                break
+        features = []
+        for map in feature_maps:
+            upsample = F.interpolate(map, size=size, mode='bilinear', align_corners=True)
+            features.append(upsample)
+        outputs = torch.cat(features, 1)
+        return outputs
---
+++ selection/vlad.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+
+class VLAD:
+    def __init__(self, n_words=64, pca_dim=128):
+        self.n_words = n_words
+        self.pca = PCA(n_components=pca_dim)
+        self.kmeans = KMeans(n_clusters=n_words, n_init=1, verbose=0)
+
+    def fit_pca_kmeans(self, embeddings_sample):
+        """
+        Given samples of embeddings, fit a PCA to reduce the dimension and a
+        kmeans to get the visual words.
+        :param embeddings_sample: aggregated random samples of pixel embeddings
+        from all the images to be select
+        """
+        print(f"shape of sampled pixel embeddings before PCA and KMeans:\n{embeddings_sample.shape}")
+        embeddings_sample = self.pca.fit_transform(embeddings_sample)
+        print("--- training kmeans ---")
+        self.kmeans.fit(embeddings_sample)
+        print("--- finished ---")
+
+    def get_vlad_repr(self, embeddings):
+        """
+        Compute VLAD representation for the embeddings of a single image. The
+        VLAD representation gives the mean residual vector of the visual words.
+        :param embeddings: pixel embeddings of a single image with shape (W*H, embedding_dim)
+        :return: VLAD representation with shape (n_words, pca_dim)
+        """
+        embeddings = self.pca.transform(embeddings)
+        predictions = self.kmeans.predict(embeddings)
+        cluster_centers = self.kmeans.cluster_centers_
+        vlad_repr = np.zeros_like(cluster_centers)
+        for i in range(0, predictions.shape[0], 10000):
+            residuals = embeddings[i:i+10000, :] - cluster_centers[predictions[i:i+10000], :]
+            for c in range(cluster_centers.shape[0]):
+                vlad_repr[c, :] += residuals[predictions[i:i+10000]==c, :].sum(axis=0)
+        return vlad_repr
+
+
+if __name__ == '__main__':
+    sample = np.random.rand(20000, 512)
+    embeddings = np.random.rand(312180, 512)
+    vlad = VLAD()
+    vlad.fit_pca_kmeans(sample)
+    print(vlad.get_vlad_repr(embeddings).shape)
